5c54f543a88cb8f23fe3406f3237cc440a5ba0cc
Ericgacoki <gacokieric@gmail.com>
2020-12-19 18:55:51 +0300

    Created 2 Activities

---
+++ app/src/main/java/com/ericg/androidw3/NewYearActivity.kt
@@ -0,0 +1,11 @@
+package com.ericg.androidw3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewYearActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_year)
+    }
+}
