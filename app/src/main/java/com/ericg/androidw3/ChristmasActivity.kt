9c8b83641d672cfec50748f6a7f75acf73d5c157
Ericgacoki <gacokieric@gmail.com>
2020-12-19 19:39:13 +0300

    wrote code for ChristmasActivity.kt and documented using block comments

--- app/src/main/java/com/ericg/androidw3/ChristmasActivity.kt
+++ app/src/main/java/com/ericg/androidw3/ChristmasActivity.kt
@@ -1,11 +1,42 @@
 package com.ericg.androidw3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.ericg.androidw3.databinding.ActivityChristmasBinding
 
 class ChristmasActivity : AppCompatActivity() {
+    /**
+     * create a mutable variable that holds the dataBinding object.
+     * get its value and init it to an immutable variable.
+     * initialize the mutable var in onCreate() method.
+     * */
+
+    private var _christmasBinding: ActivityChristmasBinding? = null
+    private val christmasBinding get() = _christmasBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_christmas)
+        _christmasBinding = DataBindingUtil.setContentView(this, R.layout.activity_christmas)
+
+        /** safely set clickListener and navigate using an Intent*/
+
+        val intentToNewYearActivity = Intent(this, NewYearActivity::class.java)
+        christmasBinding?.button1?.setOnClickListener {
+            startActivity(intentToNewYearActivity)
+            finish()
+
+            /** finish() clears activity from the back stack so that the user can not navigate
+             *back by pressing the device back button*/
+        }
+    }
+
+    /**
+     *set binding object variable to null to avoid memory leaks */
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _christmasBinding = null
     }
 }
